775f0b15ef67828cf31f782e40aec02846fd6e1c
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-30 10:15:38 +0700

    Add header on layout component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import CurrentUser from './current-user'
+import { Layout, Space } from 'antd'
 
 export default function Header() {
+  const headerStyles: React.CSSProperties = {
+    backgroundColor: '#fff',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    padding: '0px 24px',
+    height: '64px',
+    position: 'sticky',
+    top: 0,
+    zIndex: 999,
+  }
+
   return (
     <div>
-      <CurrentUser />
+      <Layout.Header style={headerStyles}>
+        <Space align='center' size='middle'>
+          <CurrentUser />
+        </Space>
+      </Layout.Header>
     </div>
   )
 }
