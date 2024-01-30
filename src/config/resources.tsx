674941a202fa5ec1b5545d17c43dcb8d7397fc9d
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-30 10:29:13 +0700

    Add resources to config

---
+++ src/config/resources.tsx
@@ -0,0 +1,40 @@
+import type { IResourceItem } from '@refinedev/core'
+
+import {
+  DashboardOutlined,
+  ProjectOutlined,
+  ShopOutlined,
+} from '@ant-design/icons'
+import React from 'react'
+
+export const resources: IResourceItem[] = [
+  {
+    name: 'dashboard',
+    list: '/',
+    meta: {
+      label: 'Dashboard',
+      icon: <DashboardOutlined rev={undefined} />,
+    },
+  },
+  {
+    name: 'companies',
+    list: '/companies',
+    show: '/companies/:id',
+    create: '/companies/new',
+    edit: '/companies/edit/:id',
+    meta: {
+      label: 'Companies',
+      icon: <ShopOutlined rev={undefined} />,
+    },
+  },
+  {
+    name: 'tasks',
+    list: '/tasks',
+    create: '/tasks/new',
+    edit: '/tasks/edit/:id',
+    meta: {
+      label: 'Tasks',
+      icon: <ProjectOutlined rev={undefined} />,
+    },
+  },
+]
