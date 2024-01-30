2d9d8c95b67daa4a8c02f4d36493b9655e665b55
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-30 19:47:05 +0700

    Add total-count-card to home component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,65 @@
-import { Row, Col } from 'antd'
-
 import React from 'react'
-import { DealsChart, UpcomingEvents } from '../../components'
+import { Row, Col } from 'antd'
+import {
+  DashboardTotalCountCard,
+  DealsChart,
+  UpcomingEvents,
+} from '../../components'
+import { useCustom } from '@refinedev/core'
+import { DashboardTotalCountsQuery } from '../../graphql/types'
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from '../../graphql/queries'
 
 export function Home() {
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: '',
+    method: 'get',
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY,
+    },
+  })
+
   return (
     <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource='companies'
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource='contacts'
+            isLoading={isLoading}
+            totalCount={data?.data.contacts.totalCount}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource='deals'
+            isLoading={isLoading}
+            totalCount={data?.data.deals.totalCount}
+          />
+        </Col>
+      </Row>
+
       <Row gutter={[32, 32]} style={{ marginTop: '32px' }}>
         <Col xs={24} sm={24} xl={8} style={{ height: '460px' }}>
           <UpcomingEvents />
         </Col>
-        <Col xs={24} sm={24} xl={8} style={{ height: '460px' }}>
+        <Col xs={24} sm={24} xl={16} style={{ height: '460px' }}>
           <DealsChart />
         </Col>
+      </Row>
+
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: '32px',
+        }}
+      >
+        <Col xs={24}></Col>
       </Row>
     </div>
   )
