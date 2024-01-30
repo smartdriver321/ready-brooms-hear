08545281baa980a490560717d468405f4188e24a
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-30 14:00:44 +0700

    Add skeleton component (Not fixed yet)

---
+++ src/components/home/deals-chart.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function DealsChart() {
+  return <div>DealsChart</div>
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,19 @@
+import { Row, Col } from 'antd'
+
 import React from 'react'
+import { DealsChart, UpcomingEvents } from '../../components'
 
 export function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <Row gutter={[32, 32]} style={{ marginTop: '32px' }}>
+        <Col xs={24} sm={24} xl={8} style={{ height: '460px' }}>
+          <UpcomingEvents />
+        </Col>
+        <Col xs={24} sm={24} xl={8} style={{ height: '460px' }}>
+          <DealsChart />
+        </Col>
+      </Row>
+    </div>
+  )
 }
