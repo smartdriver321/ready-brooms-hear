ba20d97c641652de061c734c2f4cf37b7f015b2c
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-30 17:49:18 +0700

    Add deals-chart to home component

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,5 +1,84 @@
 import React from 'react'
+import { useList } from '@refinedev/core'
+import { Card } from 'antd'
+import { DollarOutlined } from '@ant-design/icons'
+import { Area, AreaConfig } from '@ant-design/plots'
+import { DASHBOARD_DEALS_CHART_QUERY } from '../../graphql/queries'
+import { Text } from '../text'
+import { mapDealsData } from '../../utilities/utilities/helpers'
+import { GetFieldsFromList } from '@refinedev/nestjs-query'
+import { DashboardDealsChartQuery } from '../../graphql/types'
 
 export function DealsChart() {
-  return <div>DealsChart</div>
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource: 'dealStages',
+    filters: [
+      {
+        field: 'title',
+        operator: 'in',
+        value: ['WON', 'LOSt'],
+      },
+    ],
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+    },
+  })
+
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data)
+  }, [data?.data])
+
+  const config: AreaConfig = {
+    data: dealData,
+    xField: 'timeText',
+    yField: 'value',
+    isStack: false,
+    seriesField: 'state',
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    yAxis: {
+      tickCount: 4,
+      label: {
+        formatter: (v: string) => {
+          return `$${Number(v) / 1000}k`
+        },
+      },
+    },
+    tooltip: {
+      formatter: (data) => {
+        return {
+          name: data.state,
+          value: `$${Number(data.value) / 1000}k`,
+        }
+      },
+    },
+  }
+
+  return (
+    <Card
+      style={{ height: '100%' }}
+      headStyle={{ padding: '8px 16px' }}
+      bodyStyle={{ padding: '24px 24px 0 24px' }}
+      title={
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px',
+          }}
+        >
+          <DollarOutlined />
+          <Text size='sm' style={{ marginLeft: '0.5rem' }}>
+            Deals
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
+  )
 }
