69f1438c2c494d8ad541e5523c8a7d56bf3eaa1a
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-31 21:25:51 +0700

    Add edit functionality to company page

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { Home, Register, Login, ForgotPassword, CompanyList } from './pages/'
 import Layout from './components/layout'
 import { resources } from './config/resources'
 import Create from './pages/company/create'
+import Edit from './pages/company/edit'
 
 export default function App() {
   return (
@@ -59,6 +60,7 @@ export default function App() {
                   <Route path='companies'>
                     <Route index element={<CompanyList />} />
                     <Route path='new' element={<Create />} />
+                    <Route path='edit/:id' element={<Edit />} />
                   </Route>
                 </Route>
               </Routes>
